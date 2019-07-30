899a76b709d8e69d7876e7d1c5244d2417213345
David Benavidez <benavidez_david13@gmail.com>
2019-07-29 22:18:57 -0700

    layout

--- map-app/src/App.js
+++ map-app/src/App.js
@@ -4,24 +4,23 @@ import './App.css';
 
 class App extends Component {
 
-  state = {
-    viewport: {
-      width: "100vw",
-      height: "100vh",
-      latitude: 33.912470,
-      longitude: -118.202310,
-      zoom: 13
-    }
-  };
+
 
   render() {
-    const token = "pk.eyJ1IjoiZGF2aWRiZW5hdmlkZXoiLCJhIjoiY2p5bHc2OHQ1MGN4ajNnbnRrcTJ0ZGRmNCJ9.Jko64ip87P7mKYiOTECKng"
     return (
-      <ReactMapGL
-        mapboxApiAccessToken={token}
-        {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({ viewport })}
-      />
+      <div className="App">
+        <div className="nav">
+          yes
+          </div>
+        <div className="left-side">
+          yes
+        </div>
+        <div className="right-side">
+          yes
+          </div>
+
+      </div>
+
     );
   }
 
